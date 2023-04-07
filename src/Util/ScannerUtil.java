1a6e02692647347b8dc2e807c77895bb171ad52c
Kim <98963260+oanhhkim@users.noreply.github.com>
2023-04-07 23:02:51 +0700

    Delete src directory

--- src/Util/ScannerUtil.java
+++
@@ -1,8 +0,0 @@
-package Util;
-
-import java.util.Scanner;
-
-public class ScannerUtil {
-  public static final Scanner SCANNER = new Scanner(System.in);
-
-}
--- src/entity/Shape.java
+++
@@ -1,8 +0,0 @@
-package entity;
-
-public abstract class Shape {
-
-  public abstract void input();
-  public abstract void perimeter();
-  public abstract void area();
-}
